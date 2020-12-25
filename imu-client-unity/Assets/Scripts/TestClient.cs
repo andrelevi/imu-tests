beccd3b63cf93d820715a7ec075a87bd3fbcd414
Andre Levi <andrelevi09@gmail.com>
2020-12-24 21:59:13 -0800

    Get working communication between server and client

--- imu-client-unity/Assets/Scripts/TestClient.cs
+++ imu-client-unity/Assets/Scripts/TestClient.cs
@@ -1,34 +1,82 @@
 ﻿using System;
+using System.IO;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
 
 public class TestClient : MonoBehaviour
 {
-    private const string ServerIp = "127.0.0.1";
-    private const int Port = 5005;
+    [Header("Network Settings")]
+    public string LocalHost = "127.0.0.1";
+    public string RemoteHost = "192.168.x.x";
+    public bool UseRemoteHost;
+    public int Port = 5005;
 
-    // Start is called before the first frame update
+    [Header("Run-time Data")]
+    private TcpClient TcpClient;
+    private NetworkStream NetworkStream;
+    private StreamWriter StreamWriter;
+    private float _timeOfLastCheck;
+    
+    private string Host => UseRemoteHost ? RemoteHost : LocalHost;
+
     private void Start()
     {
-        //---data to send to the server---
-        var textToSend = DateTime.Now.ToString();
+        TcpClient = new TcpClient();
+
+        if (ConnectToHost())
+        {
+            Debug.Log($"Connected to: {Host}:{Port}");
+        }
+    }
+
+    private void Update()
+    {
+        if (Time.time - _timeOfLastCheck < 0.1f)
+        {
+            return;
+        }
+
+        _timeOfLastCheck = Time.time;
         
-        Debug.Log($"Connecting to: {ServerIp}:{Port}");
+        if (!TcpClient.Connected)
+        {
+            ConnectToHost();
+        }
+        else
+        {
+            var stream = TcpClient.GetStream();
+            var bytesToRead = new byte[TcpClient.ReceiveBufferSize];
+            var bytesRead = stream.Read(bytesToRead, 0, this.TcpClient.ReceiveBufferSize);
+            Debug.Log("Received: " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+        }
+    }
 
-        //---create a TCPClient object at the IP and port no.---
-        var client = new TcpClient(ServerIp, Port);
-        var nwStream = client.GetStream();
-        var bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+    private bool ConnectToHost()
+    {
+        try
+        {
+            TcpClient.Connect(Host, Port);
+            NetworkStream = TcpClient.GetStream();
+            StreamWriter = new StreamWriter(NetworkStream);
+            
+            var sendBytes = Encoding.UTF8.GetBytes("Hello, I am client.");
+            TcpClient.GetStream().Write(sendBytes, 0, sendBytes.Length);
+            
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e);
+            return false;
+        }
+    }
 
-        //---send the text---
-        Debug.Log("Sending : " + textToSend);
-        nwStream.Write(bytesToSend, 0, bytesToSend.Length);
-
-        //---read back the text---
-        var bytesToRead = new byte[client.ReceiveBufferSize];
-        var bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
-        Debug.Log("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
-        //client.Close();
+    private void OnApplicationQuit()
+    {
+        if (TcpClient != null && TcpClient.Connected)
+        {
+            TcpClient.Close();
+        }
     }
 }
--- imu-server/imu-server.py
+++ imu-server/imu-server.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
+import time
 import socket
 import argparse
 
 parser = argparse.ArgumentParser(description='Start the IMU polling and TCP server.')
-#parser.add_argument('--filter', action='store', type=str, required=True)
+parser.add_argument('--imu', action='store', type=str, default='MPU9250')
+parser.add_argument('--filter', action='store', type=str, default='kalman')
 parser.add_argument('--port', action='store', type=str, default=5005)
 parser.add_argument('--ip', action='store', type=str, default='127.0.0.1')
-parser.add_argument('--filter', action='store', type=str, default='kalman')
 
 args = parser.parse_args()
 
@@ -15,22 +16,26 @@ TCP_IP = args.ip
 TCP_PORT = args.port
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
-print(f'IMU Server')
-print(f'Starting TCP server: {TCP_IP}:{TCP_PORT}...\n')
-
-#data = 'hello'
+print(f'IMU Server\n')
+print(f'Starting TCP server at: {TCP_IP}:{TCP_PORT}\n')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
-print(f'Connection address: {addr}') 
+print(f'Client connnected: {addr}') 
+
 while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print(f'received data: {data}') 
-    conn.send(data)  # echo
+    data = b'hello'
+    print(data)
+    conn.send(data)
+    time.sleep(1)
+
+    #data = conn.recv(BUFFER_SIZE)
+    #if not data: break
+    #print(f'received data: {data}') 
+    #conn.send(data)  # echo
     #conn.close()
 
 
