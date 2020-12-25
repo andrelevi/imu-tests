451cb7975362f3dfb58b064f2f8c29ead6cdfcf1
Andre Levi <andrelevi09@gmail.com>
2020-12-24 21:22:24 -0800

    Set up test MPU9250 scripts

--- imu-server/imu/MPU9250.py
+++ imu-server/imu/MPU9250.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # https://maker.pro/raspberry-pi/tutorial/how-to-interface-an-imu-sensor-with-a-raspberry-pi
 import FaBo9Axis_MPU9250
 import time
@@ -8,20 +10,22 @@ mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 try:
     while True:
         accel = mpu9250.readAccel()
-        print " ax = " , ( accel['x'] )
-        print " ay = " , ( accel['y'] )
-        print " az = " , ( accel['z'] )
+        print('ax: ' + str(accel['x']))
+        print('ay: ' + str(accel['y']))
+        print('az: ' + str(accel['z']))
+        print('')
 
         gyro = mpu9250.readGyro()
-        print " gx = " , ( gyro['x'] )
-        print " gy = " , ( gyro['y'] )
-        print " gz = " , ( gyro['z'] )
+        print('gx: ' + str(gyro['x']))
+        print('gy: ' + str(gyro['y']))
+        print('gz: ' + str(gyro['z']))
+        print('')
 
         mag = mpu9250.readMagnet()
-        print " mx = " , ( mag['x'] )
-        print " my = " , ( mag['y'] )
-        print " mz = " , ( mag['z'] )
-        print
+        print('mx: ' + str(mag['x']))
+        print('my: ' + str(mag['y']))
+        print('mz: ' + str(mag['z']))
+        print('')
 
         time.sleep(0.1)
 
--- imu-server/install.sh
+++ imu-server/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo pip install FaBo9Axis_MPU9250
+#sudo pip install FaBo9Axis_MPU9250
+
+sudo pip3 install imusensor
+sudo pip3 install easydict
