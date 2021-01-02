3a0aa85526e3b35215b19b334a8b70e8cfda4fd2
Andre Levi <andrelevi09@gmail.com>
2021-01-01 21:53:48 -0800

    Set up Madgwick filter

--- imu-server/imu/MPU9250.py
+++ imu-server/imu/MPU9250.py
@@ -8,29 +8,39 @@ import os
 import smbus
 
 from imusensor.MPU9250 import MPU9250
+from imusensor.filters import madgwick
+from imusensor.filters import kalman
 
 def poll_imu_sensor_data(run_event, poll_frequency, is_verbose):
+    sensorfusion = madgwick.Madgwick(0.5)
+    #sensorfusion = kalman.Kalman()
+
     address = 0x68
     bus = smbus.SMBus(1)
     imu = MPU9250.MPU9250(bus, address)
     imu.begin()
 
+    currTime = time.time()
+    print_count = 0
+
     while run_event.is_set():
         imu.readSensor()
-        imu.computeOrientation()
+        for i in range(10):
+            newTime = time.time()
+            dt = newTime - currTime
+            currTime = newTime
 
-        #lists = [imu.AccelVals, imu.GyroVals, imu.MagVals]
-        #flattened_list = [item for sublist in lists for item in sublist]
+            sensorfusion.updateRollPitchYaw(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2], imu.GyroVals[0], \
+                                        imu.GyroVals[1], imu.GyroVals[2], imu.MagVals[0], imu.MagVals[1], imu.MagVals[2], dt)
 
-        flattened_list = [imu.roll, imu.pitch, imu.yaw]
+        flattened_list = [sensorfusion.roll, sensorfusion.pitch, sensorfusion.yaw]
         flattened_list = map(str, flattened_list)
 
         thread_variables.imu_sensor_data = ','.join(flattened_list)
 
-        if is_verbose:
-            print ("Accel x: {0} ; Accel y : {1} ; Accel z : {2}".format(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2]))
-            print ("Gyro x: {0} ; Gyro y : {1} ; Gyro z : {2}".format(imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2]))
-            print ("Mag x: {0} ; Mag y : {1} ; Mag z : {2}".format(imu.MagVals[0], imu.MagVals[1], imu.MagVals[2]))
-            print ("roll: {0} ; pitch : {1} ; yaw : {2}".format(imu.roll, imu.pitch, imu.yaw))
+        if is_verbose and print_count == 2:
+            print ("mad roll: {0} ; mad pitch : {1} ; mad yaw : {2}".format(sensorfusion.roll, sensorfusion.pitch, sensorfusion.yaw))
+            print_count = 0
 
+        print_count = print_count + 1
         time.sleep(poll_frequency)
